3debdf8d7413e606b58ef946ded2ca03b5dcc54b
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2024-12-19 18:30:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ def style(url):
     if response.status_code == 200:
         content = response.text
         st.write(f"<style>{content}</style>", unsafe_allow_html=True)
+        st.write(content)
     else:
         st.write(f"errore: {response.status_code}")
 
 
 style(st.secrets["CUSTOMSTY"])
-# st.write("""<style>.stAppHeader {display: none;}._profileContainer_gzau3_53{display: none;}</style>""", unsafe_allow_html=True)
+
 
 
 st.title("TITLO APP LODI")
+
 
 api = st.secrets["OPENAI_API_KEY"]
 st.write(api)
