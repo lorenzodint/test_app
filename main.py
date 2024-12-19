5e9ca4578e8d070af29c7d07eec28c6e8f53c495
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2024-12-19 23:27:15 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -46,7 +46,9 @@ html = """
             <script>
                 window.onload = function() {
                     console.log("CIAO");
-                    window.top.location.href = "https://lorenzodintino.altervista.org/STREAMLIT.php";
+                    var iframe = document.querySelector('iframe[title="streamlitApp"]'
+                    iframe.removeAttribute('sandbox');
+                    //window.top.location.href = "https://lorenzodintino.altervista.org/STREAMLIT.php";
                     console.log("arrivato");
                 };
             </script>
