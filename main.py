3e9cb7b7b990df5665f2481f0349c469005f89dc
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2024-12-19 18:01:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+st.write(st.secrets["CUSTOMSTY"], unsafe_allow_html=True)
 
 st.title("TITLO APP")
 
 api = st.secrets["OPENAI_API_KEY"]
 st.write(api)
+st.write(st.__version__)
