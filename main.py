af221181f3c33fc7faf5e536734dc3ab9ef46621
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2024-12-19 20:11:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import os
 import requests
 from streamlit_navigation_bar import st_navbar
+from streamlit_option_menu import option_menu
 
 st.set_page_config(layout="wide")
 
@@ -20,5 +21,18 @@ style(st.secrets["CUSTOMSTY"])
 
 
 
-page = st_navbar(["HOME", "DOCUMENTI", "ESEMPI", "COMMUNITY", "CONTATTI"])
-st.write(page)
+selected = option_menu(
+    menu_title=None,
+    options=["home", "progetti", "contatti"],
+    icons=["house", "book", "envelope"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
+)
+
+if selected == "home":
+    st.title("HOME")
+if selected == "progetti":
+    st.title("PROGETTI")
+if selected == "contatti":
+    st.title("CONTATTI")
