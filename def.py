fda678a0af72c24f8dea767fa03abe44feb0e465
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2024-12-20 00:05:23 +0100

    Add files via upload

--- def.py
+++ def.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+url = "https://lodi-def.streamlit.app/~/+/"
 
 st.set_page_config(layout="wide")
 
@@ -8,4 +9,6 @@ st.set_page_config(layout="wide")
 st.title("Lodi APP")
 
 st.write(st.secrets["OPENAI_API_KEY"])
+
+st.write(url)
 
