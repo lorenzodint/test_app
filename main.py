c0fb1f51dd909d47526482716058bf186dbeea5b
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2024-12-19 18:19:29 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import os
+import requests
 
 st.set_page_config(layout="wide")
 
 # IMPORT CSS
-def local_css(fileName):
-    with open(fileName) as f:
-        st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+def style(url):
+    response = requests.post(url)
+    if response.status_code == 200:
+        content = response.text
+        st.write(content)
+    else:
+        st.write(f"errore: {response.status_code}")
 
 
-local_css(st.secrets["CUSTOMSTY"])
+style(st.secrets["CUSTOMSTY"])
 
 st.title("TITLO APP LODI")
 
