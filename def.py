1f9d4112821c7153bc58a283769ffd88f245c02d
Lorenzo D'intino <64317922+lorenzodint@users.noreply.github.com>
2024-12-20 18:25:42 +0100

    Add files via upload

--- def.py
+++ def.py
@@ -1,35 +1,49 @@
-import streamlit as st
-import streamlit.components.v1 as components
-# import src.functions as func
-import requests
-import importlib.util
+
+import src.functions
 import sys
+import importlib.util
+import requests
+import streamlit.components.v1 as components
+import streamlit as st
+print("sono main")
+# import src.functions as func
 
 st.set_page_config(layout="wide")
 
+files = [
+    st.secrets['FUNZIONI'],
+    st.secrets['FU'],
+]
 
+modules = {}
 
-response = requests.get(st.secrets["IMPORT_FILE"])
-
-namespace = {}
-
-exec(response.text, namespace)
+for file in files:
+    response = requests.get(file)
+    file_name = file.split("/")[-1]
+    namespace = {}
+    
+    exec(response.text, namespace)
+    
+    module_name = file_name.replace('.py', '')
+    modules[module_name] = namespace
+    
+    
+    
+    
+# exec(response.text, namespace)
 
 url = "https://lodi-def.streamlit.app/~/+/"
-
-
-
 
 st.title("Lodi APP")
 
 st.write(st.secrets["OPENAI_API_KEY"])
 
-
-         
 js = """
- 
-"""
+    
+    """
 components.html(js, height=0)
 
-namespace['scrivi']("ciao")
-# scrivi("ciao")
+# namespace['scrivi']("ciao")
+
+modules["functions"]["scrivi"]()
+modules["fu"]["saluta"]()
